741884e2c988275e0f7d32af3a6a3baa5728e970
eichann <k.abe.in401@gmail.com>
2015-02-14 01:05:35 +0900

    プロトタイプ

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,11 +1,23 @@
 class ImpressionsController < ApplicationController
+
   def index
     @all_count = Impression.count
     @impressions = Impression.all.map { |i| i.url.split("=")[1]}
   end
 
   def create
-    Impression.create(url: params[:url], status: 0)
-    redirect_to action: :index
+    @impression = Impression.where(url: url_params[:url]).first_or_initialize
+
+    if @impression.save
+      redirect_to @imperssion, notice: 'ok!'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def url_params
+    params.permit(:url)
   end
 end
--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,2 +1,10 @@
 class Impression < ActiveRecord::Base
+  validates :url, presence: true
+  validate :add_error_message
+
+  def add_error_message
+    if url.empty?
+      errors.add(:name, "oh, plz tell me your recommend!!")
+    end
+  end
 end
