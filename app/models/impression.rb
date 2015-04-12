22378770147d652734a453046e010c10117879d7
eichann <k.abe.in401@gmail.com>
2015-04-12 18:17:26 +0900

    validate "not" youtube url

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,4 +1,5 @@
 class Impression < ActiveRecord::Base
+  VALID_YOUTUBE_URL_REGEX = /\A.*youtube.*/  #メールアドレスフォーマットの検証（完璧な正規表現ではない）
   belongs_to :channel
-  validates :url, presence: true
+  validates :url, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
 end
