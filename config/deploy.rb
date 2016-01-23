fbc5cfdc943a717eb2c2d5f30276a76d02976eac
eichann <k.abe.in401@gmail.com>
2016-01-23 22:46:03 +0900

    add app role

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,7 @@ namespace :deploy do
   end
   after :publishing, :restart
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:web, :app), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       #       # within release_path do
       #             #   execute :rake, 'cache:clear'
