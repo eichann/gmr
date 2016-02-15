8bc5e0536a084147503e231d50e7ab11f47566fd
eichann <k.abe.in401@gmail.com>
2016-02-16 01:17:10 +0900

    add carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,17 +2,17 @@ CarrierWave.configure do |config|
     config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'ACCESS_KEY_ID',
-    aws_secret_access_key: 'SECRET_ACCESS_KEY',
-    region: 'ap-northeast-1'
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'us-east-1'
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'on-air.tokyo'
-        config.asset_host = 'https://s3.amazonaws.com/on-air.tokyo'
+        config.fog_directory  = 'on-air-tokyo'
+        config.asset_host = 'https://s3.amazonaws.com/on-air-tokyo'
     when 'production'
         config.fog_directory  = 'on-air.tokyo'
-        config.asset_host = 'https://s3.amazonaws.com/on-air.tokyo'
+        config.asset_host = 'https://s3.amazonaws.com/on-air-tokyo'
     end
 end
